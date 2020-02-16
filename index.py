67424f67747fc1023c1eca90a1073d6388789eb0
kalinin <prozaik81-2@yandex.ru>
2020-02-16 23:20:22 +0300

    many-to-many-complete

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import random
 from sqlalchemy import create_engine, Table, MetaData, select, Column, String, Integer, SmallInteger, ForeignKey
 from sqlalchemy.sql import and_, or_, not_
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship, backref
 
 
 engine = create_engine('sqlite:///db.sqlite3')
@@ -14,36 +14,32 @@ session = Session()
 
 class Author(Base):
     __tablename__ = 'authors'
-
     id = Column(Integer, primary_key=True)
     first_name = Column(String(100), nullable=False)
     last_name = Column(String(100), nullable=False)
     books = relationship("Book", secondary='association_table')
 
+    def __repr__(self):
+        return "Author:" + self.first_name
+
 
 class Book(Base):
     __tablename__ = 'books'
-
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     copyright = Column(SmallInteger, nullable=False)
     authors = relationship("Author", secondary='association_table')
 
+    def __repr__(self):
+        return "Book:" + self.title + str(self.copyright)
+
 
 class AssociationTable(Base):
     __tablename__ = 'association_table'
-
-    author_id = Column(
-        Integer, 
-        ForeignKey('authors.id'), 
-        primary_key = True
-    )
-
-    book_id = Column(
-        Integer, 
-        ForeignKey('books.id'), 
-        primary_key = True
-    )  
+    author_id = Column(Integer, ForeignKey('authors.id'), primary_key = True)
+    book_id = Column(Integer, ForeignKey('books.id'), primary_key = True)
+    author = relationship("Author", backref=backref("association_table", cascade="all, delete-orphan"))
+    book = relationship("Book", backref=backref("association_table", cascade="all, delete-orphan"))
 
 
 Base.metadata.create_all(engine)
@@ -62,14 +58,17 @@ a1.books.append(b1)
 a1.books.append(b2)
 a2.books.append(b3)
 
-b1.authors.append(a1)
-b2.authors.append(a1)
-b3.authors.append(a2)
 b4.authors.append(a1)
 b4.authors.append(a2)
 
-session.add_all(a1, a2)
-session.add_all(b1, b2, b3, b4)
+session.add(a1)
+session.add(a2)
+
+session.add(b1)
+session.add(b2)
+session.add(b3)
+session.add(b4)
+
 session.commit()
 
 
@@ -77,9 +76,10 @@ authors_result = []
 authors = session.query(Author).all()
 for author in authors:
     authors_result.append({'id': author.id, 'first_name': author.first_name})
+print('authors:', authors_result, '\n\n')
 
 
 books = session.query(Book).all()
 for book in books:
-    author = next(filter(lambda a: a['id'] == book.author_id, authors_result))
-    print(book.id, book.title, book.copyright, author['first_name'])
+    print(book, '---', book.id, book.title, book.copyright, '---', book.authors)
+
